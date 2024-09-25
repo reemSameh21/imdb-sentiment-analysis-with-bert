7f0c880c35c15c1b1bd6168f0ea98631c2efc063
reemSameh21 <169095593+reemSameh21@users.noreply.github.com>
2024-09-25 17:18:20 +0300

    Update model.py

--- model.py
+++ model.py
@@ -38,10 +38,10 @@ def train_model():
         evaluation_strategy="epoch",
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
-        num_train_epochs=5, 
+        num_train_epochs=4, 
         logging_dir="./logs",
         logging_steps=10,
-        learning_rate=1e-5,   
+        learning_rate=1e-6,   
     )
 
     # Trainer setup
