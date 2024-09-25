1c1d94d28725eaef7934002eb0b549078b2c9c09
reemSameh21 <169095593+reemSameh21@users.noreply.github.com>
2024-09-25 19:11:25 +0300

    Last Update

--- test_model.py
+++ test_model.py
@@ -2,4 +2,4 @@ from model import train_model
 
 def test_model_accuracy():
     accuracy = train_model()
-    assert accuracy['eval_accuracy'] > 0.5, "Model accuracy is too low!"
+    assert accuracy['eval_accuracy'] > 0.6, "Model accuracy is too low!"
